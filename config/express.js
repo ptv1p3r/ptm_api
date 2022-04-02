073f39fc58caf0d7373dd750d72d831094b05bde
Pedro Roldan <pedroroldan.devoteam@edp.com>
2022-04-02 19:13:32 +0100

    housekeeping

--- config/express.js
+++ config/express.js
@@ -6,7 +6,7 @@ const consign    = require('consign');
 module.exports = () => {
     const app = express();
 
-    // SETANDO VARIÁVEIS DA APLICAÇÃO
+    // VARIÁVEIS DA APLICAÇÃO
     app.set('port', process.env.PORT || config.get('server.port'));
 
     // MIDDLEWARES
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./config/express')();
 const port = app.get('port');
 
-// RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
+// REST SERVER START
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
+    console.log(`Project Tree Management REST API on Port ${port}`)
 });
