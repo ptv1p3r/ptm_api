5cf2056d8c5393b9fdd941da8fb5a7d1c62ced30
Pedro Roldan <pedroroldan.devoteam@edp.com>
2022-04-03 12:54:45 +0100

    ADD Midleware urlencode added
    ADD Midleware cors added

--- config/express.js
+++ config/express.js
@@ -2,6 +2,12 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const config     = require('config');
 const consign    = require('consign');
+const mariadb    = require('mariadb');
+const cors       = require("cors");
+
+const corsOptions = {
+    origin: "*"
+};
 
 module.exports = () => {
     const app = express();
@@ -10,7 +16,12 @@ module.exports = () => {
     app.set('port', process.env.PORT || config.get('server.port'));
 
     // MIDDLEWARES
+    // parse requests of content-type - application/json
     app.use(bodyParser.json());
+    // parse requests of content-type - application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
+    // set cors
+    app.use(cors(corsOptions));
 
     // ENDPOINTS
     consign({cwd: 'api'})
