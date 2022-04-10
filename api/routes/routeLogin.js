96e82536c98ac85fd2f840c59c01617e48350040
Pedro Roldan <pedroroldan.devoteam@edp.com>
2022-04-10 12:51:54 +0100

    Housekeeping

--- api/routes/routeLogin.js
+++ api/routes/routeLogin.js
@@ -1,8 +1,8 @@
 module.exports = app => {
     const controller = app.controllers.controllerLogin;
 
-    app.route('/api/v1/login').get(controller.login);
+    app.route('/api/v1/login').post(controller.login);
     app.route('/api/v1/logout').get(controller.logout);
-    app.route('/api/v1/register').get(controller.logout);
+    app.route('/api/v1/register/:activationToken').get(controller.logout);
 
 }
--- api/routes/routeUsers.js
+++ api/routes/routeUsers.js
@@ -2,11 +2,11 @@ module.exports = app => {
     const controller = app.controllers.controllerUsers;
 
     app.route('/api/v1/users/list').get(controller.listAll);
-    app.route('/api/v1/users/view').get(controller.viewUser);
+    app.route('/api/v1/users/view/:userId').get(controller.viewUser);
 
     app.route('/api/v1/users/create').post(controller.createUser);
 
-    app.route('/api/v1/users/edit').get(controller.editUser);
+    app.route('/api/v1/users/edit//:userId').get(controller.editUser);
     app.route('/api/v1/users/delete/:userId').delete(controller.deleteUser);
 
 }
