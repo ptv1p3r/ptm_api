8c478ca6778ee6d44b657a12694d6a772960729b
Pedro Roldan <pedroroldan.devoteam@edp.com>
2022-04-10 13:06:51 +0100

    Housekeeping

--- api/routes/routeUsers.js
+++ api/routes/routeUsers.js
@@ -1,12 +1,29 @@
 module.exports = app => {
     const controller = app.controllers.controllerUsers;
 
+    /**
+     * Users list endpoint
+     */
     app.route('/api/v1/users/list').get(controller.listAll);
+
+    /**
+     * User details endpoint
+     */
     app.route('/api/v1/users/view/:userId').get(controller.viewUser);
 
+    /**
+     * User creation endpoint
+     */
     app.route('/api/v1/users/create').post(controller.createUser);
 
+    /**
+     * User update endpoint
+     */
     app.route('/api/v1/users/edit//:userId').get(controller.editUser);
+
+    /**
+     * User deletion endpoint
+     */
     app.route('/api/v1/users/delete/:userId').delete(controller.deleteUser);
 
 }
