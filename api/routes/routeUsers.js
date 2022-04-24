76c4bf84c05a144d1b9cb8402ee863f1b2857934
Pedro Roldan DEVOTEAM <PedroRoldan.DEVOTEAM@edp.com>
2022-04-24 19:04:59 +0100

    ADD edit user
    ADD delete user

--- api/routes/routeUsers.js
+++ api/routes/routeUsers.js
@@ -1,31 +1,34 @@
 'use strict';
 
+const { AuthenticateJWT } = require("../helpers/security")
+
 module.exports = app => {
     const controller = app.controllers.controllerUsers;
 
     /**
      * Users list endpoint
      */
-    app.route('/api/v1/users/list').get(controller.listAll);
+    app.route('/api/v1/users/list').get(AuthenticateJWT, controller.listAll);
 
     /**
      * User details endpoint
      */
-    app.route('/api/v1/users/view/:userId').get(controller.viewUser);
+    app.route('/api/v1/users/view/:userId').get(AuthenticateJWT, controller.viewUser);
 
     /**
      * User creation endpoint
      */
-    app.route('/api/v1/users/create').post(controller.createUser);
+    app.route('/api/v1/users/create').post(AuthenticateJWT, controller.createUser);
+    // TODO Add a public create user for frontend
 
     /**
      * User update endpoint
      */
-    app.route('/api/v1/users/edit//:userId').get(controller.editUser);
+    app.route('/api/v1/users/edit/:userId').put(AuthenticateJWT, controller.editUser);
 
     /**
      * User deletion endpoint
      */
-    app.route('/api/v1/users/delete/:userId').delete(controller.deleteUser);
+    app.route('/api/v1/users/delete/:userId').delete(AuthenticateJWT, controller.deleteUser);
 
 }
