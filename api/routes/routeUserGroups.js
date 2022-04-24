916b13e9fcd68472763d7a6979daaf9d15decc70
Pedro Roldan <pedroroldan.devoteam@edp.com>
2022-04-24 12:07:35 +0100

    FIX added security AuthenticateJWT to all endpoints

--- api/routes/routeUserGroups.js
+++ api/routes/routeUserGroups.js
@@ -8,26 +8,26 @@ module.exports = app => {
     /**
      * User groups list
      */
-    app.route('/api/v1/groups/list').get(controller.listAll);
+    app.route('/api/v1/groups/list').get(AuthenticateJWT, controller.listAll);
 
     /**
      * User group view by id
      */
-    app.route('/api/v1/groups/view/:groupId').get(controller.viewUserGroup);
+    app.route('/api/v1/groups/view/:groupId').get(AuthenticateJWT, controller.viewUserGroup);
 
     /**
      * User create new group
      */
-    app.route('/api/v1/groups/create').post(controller.createUserGroup);
+    app.route('/api/v1/groups/create').post(AuthenticateJWT, controller.createUserGroup);
 
     /**
      * Edit user group
      */
-    app.route('/api/v1/groups/edit/:groupId').put(controller.editUserGroup);
+    app.route('/api/v1/groups/edit/:groupId').put(AuthenticateJWT, controller.editUserGroup);
 
     /**
      * Delete user group by id
      */
-    app.route('/api/v1/groups/delete/:groupId').delete(controller.deleteUserGroup);
+    app.route('/api/v1/groups/delete/:groupId').delete(AuthenticateJWT, controller.deleteUserGroup);
 
 }
