a858d0235795bfdc19655db62f48b16cbf04cbe9
Pedro Roldan <pedroroldan.devoteam@edp.com>
2022-05-08 01:33:51 +0100

    housekeeping

--- api/controllers/controllerTrees.js
+++ api/controllers/controllerTrees.js
@@ -7,9 +7,13 @@ module.exports = app => {
     const controller = {};
 
     controller.listAll = (req, res) => res.status(200).json("List all Trees");
+
     controller.viewTree = (req, res) => res.status(200).json("View Tree");
+
     controller.createTree = (req, res) => res.status(200).json("Create Tree");
+
     controller.editTree = (req, res) => res.status(200).json("Edit Tree");
+
     controller.deleteTree = (req, res) => res.status(200).json("Delete Tree");
 
     return controller;
