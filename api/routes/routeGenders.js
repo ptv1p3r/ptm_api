7c46a8558932e122c84c865d757d14a6b55acd3d
Pedro Roldan DEVOTEAM <PedroRoldan.DEVOTEAM@edp.com>
2022-05-09 20:55:04 +0100

    housekeeping

--- api/routes/routeGenders.js
+++ api/routes/routeGenders.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const { AuthenticateJWT } = require("../helpers/security")
-
 module.exports = app => {
     const controller = app.controllers.controllerGenders;
 
-    app.route('/api/v1/genders/list').get(AuthenticateJWT, controller.listAll);
+    app.route('/api/v1/genders/list').get(controller.listAll);
 
 }
