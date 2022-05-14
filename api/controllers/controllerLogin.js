55fcc69feebcb8db272a273d35bbcabc17a3a471
Pedro Roldan <pedroroldan.devoteam@edp.com>
2022-05-14 18:04:59 +0100

    ADD controller.recover
    ADD controller.activation

--- api/controllers/controllerLogin.js
+++ api/controllers/controllerLogin.js
@@ -3,7 +3,7 @@
 const emailController = require('./../helpers/email');
 const jwt = require('jsonwebtoken');
 const responseCode = require('../helpers/httpCodesDefinitions')
-const { verifyRefreshJWT } = require("../helpers/security")
+const { verifyRefreshJWT, encryptText, decryptText } = require("../helpers/security")
 const modelUser = require('./../models/modelUsers')();
 
 module.exports = app => {
@@ -153,24 +153,121 @@ module.exports = app => {
         });
     }
 
+    /**
+     * User recover password
+     * @param req
+     * @param res
+     * @returns {Promise<*>}
+     */
     controller.recover = async (req, res) => {
         try {
             const userData = {
                 email: req.body.email.toLowerCase().trim()
             }
 
+            const user = await modelUser.getUserByEmail(userData.email);
+
+            if (!user) {
+                return res.status(responseCode.ERROR_CODE.NOT_FOUND).json({
+                    recover: false,
+                    message: "User not found!"
+                });
+            }
+
+            // encrypt user id and generate encrypted object (iv, encryptedData)
+            const encryptedData = encryptText(user[0].id);
+
+            // store encrypt key
+            const userEncryptData = {
+                id: user[0].id,
+                iv: encryptedData.iv
+            }
+            // store encryption key on user record
+            await modelUser.setUserEncryptKey(userEncryptData);
+
             await emailController.sendMail({
                 from: '"www.adoteumaarvore.pt 👻" <' + global.smtpUser + '>', // sender address
-                to: userData.email, // list of receivers
+                to: user[0].email, // list of receivers
                 subject: "User Password Recovery ✔", // Subject line
                 template: 'userPasswordRecovery', // template to use
                 context:{
                     domain: "www.adoteumaarvore.pt", // {{domain}}
+                    recoverPasswordLink: `http://127.0.0.1/register/passrecover/${user[0].id}/${encryptedData.encryptedData}`, // {{recoverPasswordLink}}
                     helpEmail: "info@adoteumaarvore.pt", // {{helpEmail}}
                 },
             });
 
             res.status(responseCode.SUCCESS_CODE.OK).end('User password recovery email sent.');
+
+        } catch (error) {
+            res.status(responseCode.ERROR_CODE.BAD_REQUEST).json({
+                activated: false,
+                code: error.code,
+                message: error.text
+            });
+        }
+    }
+
+    /**
+     * User recover password activation
+     * @param req
+     * @param res
+     * @returns {Promise<*>}
+     */
+    controller.activation = async (req, res) => {
+        try {
+            const userData = {
+                id: req.params.userId.trim(),
+                token: req.body.token.trim(),
+            }
+
+            // get user data
+            const user = await modelUser.getUserById(userData.id);
+
+            if (!user) {
+                return res.status(responseCode.ERROR_CODE.NOT_FOUND).json({
+                    recover: false,
+                    message: "User not found!"
+                });
+            }
+
+            // create decrypt object
+            const decryptPayload = {
+                iv: user[0].encryptKey,
+                encryptedData: userData.token
+            }
+
+            // encrypt user id and generate encrypted object (iv, encryptedData)
+            const decryptedData = decryptText(decryptPayload);
+            // check decrypted user id with supplied id
+            if (user[0].id !== decryptedData) {
+                return res.status(responseCode.ERROR_CODE.BAD_REQUEST).json({
+                    recover: false,
+                    message: "Encryption validation mismatch!"
+                });
+            }
+
+            // setup user payload
+            const userUpdateData = {
+                id: user[0].id,
+                body: {
+                    password: req.body.password.trim()
+                }
+            }
+            // set password
+            await modelUser.editPatchUser(userUpdateData)
+
+            // clear encryption key on user record
+            const userEncryptData = {
+                id: user[0].id,
+                iv: ""
+            }
+            await modelUser.setUserEncryptKey(userEncryptData);
+
+            res.status(responseCode.SUCCESS_CODE.OK).json({
+                recover: true,
+                message: "User password recovery completed successfully!"
+            });
 
         } catch (error) {
             res.status(responseCode.ERROR_CODE.BAD_REQUEST).json({
