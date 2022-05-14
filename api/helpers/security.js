f30182b69036b747945ae39a3711ed8853405d06
Pedro Roldan <pedroroldan.devoteam@edp.com>
2022-05-14 11:32:38 +0100

    ADD encryptText for aes-256-cbc algorithm
    ADD decryptText for aes-256-cbc algorithm

--- api/helpers/security.js
+++ api/helpers/security.js
@@ -1,5 +1,10 @@
 const jwt           = require('jsonwebtoken');
-const responseCode  = require('../helpers/httpCodesDefinitions')
+const responseCode  = require('../helpers/httpCodesDefinitions');
+const crypto        = require('crypto');
+const algorithm     = 'aes-256-cbc'; //Using AES encryption
+
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
 /**
  * Authentication of JWT Access Token
@@ -53,4 +58,37 @@ function verifyRefreshJWT(email, refreshToken) {
     }
 }
 
-module.exports = { AuthenticateJWT, verifyRefreshJWT }
+/**
+ * Encrypt supplied text with aes-256-cbc algo function
+ * @param {String} text - String to encrypt
+ * @returns {{encryptedData: string, iv: string}}
+ */
+function encryptText(text) {
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return {
+        iv: iv.toString('hex'),
+        encryptedData: encrypted.toString('hex')
+    };
+}
+
+/**
+ * Decrypt supplied text with aes-256-cbc algo function
+ * @param {Object} text - Object to decrypt
+ * @returns {string}
+ */
+function decryptText(text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+module.exports = {
+    AuthenticateJWT,
+    verifyRefreshJWT,
+    encryptText,
+    decryptText,
+}
