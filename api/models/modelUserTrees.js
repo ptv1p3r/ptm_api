94e92daf57bcae4ee9a3136b10c3eb31c66c151e
Pedro Roldan <pedroroldan.devoteam@edp.com>
2022-05-15 13:41:13 +0100

    housekeeping

--- api/models/modelUserTrees.js
+++ api/models/modelUserTrees.js
@@ -6,36 +6,6 @@ module.exports = app => {
     const model = {};
 
     /**
-     * Get user by email
-     * @param {Integer} groupId - Group Id
-     * @returns {Promise<*>}
-     */
-    model.getUserGroupById = async (groupId) => {
-        let conn;
-
-        try {
-            conn = await dbPool.getConnection();
-            // return await conn.query(`SELECT id, name, description, securityId, active,
-            //     CONVERT_TZ(dateCreated,'UTC','Europe/Lisbon') AS dateCreated,
-            //     CONVERT_TZ(dateModified,'UTC','Europe/Lisbon') AS dateModified
-            //     FROM userGroups WHERE id=${groupId}`);
-            return await conn.query(`SELECT userGroups.id, userGroups.name, userGroups.description, userGroups.securityId, userGroups.active,
-                CONVERT_TZ(userGroups.dateCreated,'UTC','Europe/Lisbon') AS dateCreated,
-                CONVERT_TZ(userGroups.dateModified,'UTC','Europe/Lisbon') AS dateModified,
-                security.homeLogin, security.admLogin, security.usersCreate, security.usersRead, security.usersUpdate, security.usersDelete,
-                security.userGroupsCreate, security.userGroupsRead, security.userGroupsUpdate, security.userGroupsDelete, security.treesCreate,
-                security.treesRead, security.treesUpdate, security.treesDelete, security.treeTypeCreate, security.treeTypeRead, security.treeTypeUpdate,
-                security.treeTypeDelete, security.treeImagesCreate, security.treeImagesRead, security.treeImagesUpdate, security.treeImagesDelete
-                FROM userGroups, security WHERE userGroups.id=${groupId} AND securityId=security.id`);
-        } catch (err) {
-            console.log("error: " + err);
-            throw err;
-        } finally {
-            if (conn) await conn.end();
-        }
-    }
-
-    /**
      * Lists user trees
      * @returns {Promise<void>}
      */
