16411b15c7250c2986a2b5345302cf465f9c27ba
Pedro Roldan DEVOTEAM <PedroRoldan.DEVOTEAM@edp.com>
2022-05-18 16:05:53 +0100

    ADD TreeImage structure

---
+++ api/models/modelTreeImages.js
@@ -0,0 +1,162 @@
+'use strict';
+
+const { dbPool, buildPatchSqlQuery } = require('./../helpers/db');
+
+module.exports = app => {
+    const model = {};
+
+    /**
+     * Public lists all trees
+     * @returns {Promise<void>}
+     */
+    model.treesPublicList = async () => {
+        let conn;
+
+        try {
+            conn = await dbPool.getConnection();
+
+            return await conn.query("SELECT tree.name AS nameCientific, tree.nameCommon, tree.description, tree.observations, " +
+                "tree.lat, tree.lng, type.name AS nameType,  type.description AS descriptionType " +
+                "FROM trees AS tree, treeType AS type " +
+                "WHERE tree.active=1 AND tree.typeId=type.id");
+        } catch (err) {
+            console.log("error: " + err);
+            throw err;
+        } finally {
+            if (conn) await conn.end();
+        }
+    }
+
+    /**
+     * Lists all trees
+     * @returns {Promise<void>}
+     */
+    model.treesListAll = async () => {
+        let conn;
+
+        try {
+            conn = await dbPool.getConnection();
+
+            return await conn.query("SELECT id, name, nameCommon, description, observations, typeId, lat, lng, active, " +
+                "CONVERT_TZ(dateCreated,'UTC','Europe/Lisbon') AS dateCreated, " +
+                "CONVERT_TZ(dateModified,'UTC','Europe/Lisbon') AS dateModified " +
+                "FROM trees");
+        } catch (err) {
+            console.log("error: " + err);
+            throw err;
+        } finally {
+            if (conn) await conn.end();
+        }
+    }
+
+    /**
+     * Get tree by id
+     * @param {String} treeId - Tree id
+     * @returns {Promise<void>}
+     */
+    model.getTreeById = async (treeId) => {
+        let conn;
+
+        try {
+            conn = await dbPool.getConnection();
+
+            return await conn.query(`SELECT id, name, nameCommon, description, observations, typeId, lat, lng, active, 
+                CONVERT_TZ(dateCreated,'UTC','Europe/Lisbon') AS dateCreated,
+                CONVERT_TZ(dateModified,'UTC','Europe/Lisbon') AS dateModified 
+                FROM trees WHERE id='${treeId}'`);
+        } catch (err) {
+            console.log("error: " + err);
+            throw err;
+        } finally {
+            if (conn) await conn.end();
+        }
+    }
+
+    /**
+     * Create a new tree
+     * @param {Object} treeData - Tree details
+     * @returns {Promise<void>}
+     */
+    model.createTree = async (treeData) => {
+        let conn;
+
+        try {
+            conn = await dbPool.getConnection();
+
+            return await conn.query("INSERT INTO trees value (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                [treeData.id, treeData.name, treeData.nameCommon, treeData.description, treeData.observations,
+                    treeData.typeId, treeData.lat, treeData.lng, treeData.active, treeData.dateCreated, treeData.dateModified]);
+        } catch (err) {
+            console.log("error: " + err);
+            throw err;
+        } finally {
+            if (conn) await conn.end();
+        }
+    }
+
+    /**
+     * Edit a tree using PUT
+     * @param {Object} treeData - Tree details
+     * @returns {Promise<void>}
+     */
+    model.editPutTree = async (treeData) => {
+        let conn;
+
+        try {
+            conn = await dbPool.getConnection();
+
+            return await conn.query(`UPDATE trees SET name='${treeData.name}', nameCommon='${treeData.nameCommon}', description='${treeData.description}',
+                 observations='${treeData.observations}', typeId=${treeData.typeId}, lat=${treeData.lat}, lng=${treeData.lng}, 
+                 active=${treeData.active}, dateModified=NOW() WHERE id='${treeData.id}'`);
+
+        } catch (err) {
+            console.log("error: " + err);
+            throw err;
+        } finally {
+            if (conn) await conn.end();
+        }
+    }
+
+    /**
+     * Edit a tree using PATCH
+     * @param treeData
+     * @returns {Promise<any>}
+     */
+    model.editPatchTree = async (treeData) => {
+        let conn;
+
+        try {
+
+            conn = await dbPool.getConnection();
+
+            return await conn.query(buildPatchSqlQuery('trees',treeData.id, treeData.body));
+
+        } catch (err) {
+            console.log("error: " + err);
+            throw err;
+        } finally {
+            if (conn) await conn.end();
+        }
+    }
+
+    /**
+     * Delete tree
+     * @param {String} treeId - Tree unique identifier
+     * @returns {*}
+     */
+    model.deleteTree = async (treeId) => {
+        let conn;
+
+        try {
+            conn = await dbPool.getConnection();
+            return await conn.query(`DELETE FROM trees WHERE id='${treeId}'`);
+        } catch (err) {
+            console.log("error: " + err);
+            throw err;
+        } finally {
+            if (conn) await conn.end();
+        }
+    }
+
+    return model;
+}
