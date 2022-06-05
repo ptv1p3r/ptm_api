1b8f4faed7d9e29844acfa2cf65c8acd4506d610
Pedro Roldan DEVOTEAM <PedroRoldan.DEVOTEAM@edp.com>
2022-06-05 13:25:13 +0100

    ADD getMessageById

--- api/models/modelMessages.js
+++ api/models/modelMessages.js
@@ -31,6 +31,33 @@ module.exports = app => {
     }
 
     /**
+     * Lists all messages for userId
+     *
+     * @param {String} userId - User unique identifier
+     * @returns {Promise<void>}
+     */
+    model.getUserMessagesListById = async (userId) => {
+        let conn;
+
+        try {
+            conn = await dbPool.getConnection();
+
+            return await conn.query(`SELECT id, subject, message, fromUser, fromName, fromEmail,
+                toUser, toName, toEmail, active,
+                CONVERT_TZ(notificationDate,'UTC','Europe/Lisbon') AS notificationDate,
+                CONVERT_TZ(receptionDate,'UTC','Europe/Lisbon') AS receptionDate,
+                CONVERT_TZ(dateCreated,'UTC','Europe/Lisbon') AS dateCreated,
+                CONVERT_TZ(dateModified,'UTC','Europe/Lisbon') AS dateModified
+                FROM messages WHERE toUser='${userId}'`);
+        } catch (err) {
+            console.log("error: " + err);
+            throw err;
+        } finally {
+            if (conn) await conn.end();
+        }
+    }
+
+    /**
      * Get message by id
      * @param {String} messageId - Message unique identifier
      * @returns {Promise<void>}
