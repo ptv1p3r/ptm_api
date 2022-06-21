8a077366cc8f64ca79039748c32096be43096b2b
Pedro Roldan DEVOTEAM <PedroRoldan.DEVOTEAM@edp.com>
2022-06-21 15:24:24 +0100

    housekeeping

--- api/helpers/httpCodesDefinitions.js
+++ api/helpers/httpCodesDefinitions.js
@@ -34,7 +34,7 @@ const SERVER_CODE = {
 
 /**
  * GENERIC MESSAGES
- * @type {{GENERIC: {INPUT_EXISTING_IMAGE: string}, ERROR: {NO_TRANSACTION_TYPE_FOUND: string, NO_TRANSACTION_FOUND: string, NO_TREE_FOUND: string, NO_TREE_TYPE_FOUND: string, NO_USER_FOUND: string, NO_USER_GROUP_FOUND: string, NO_DATA: string, NO_DATA_FOUND: string, NO_IMAGE_FOUND: string, NO_TRANSACTION_METHOD_FOUND: string, NO_SECURITY_GROUP_FOUND: string}}}
+ * @type {{GENERIC: {INPUT_EXISTING_IMAGE: string, USER_LOGOUT: string, USER_PASSWORD_RECOVERY_EMAIL: string, USER_PASSWORD_RECOVERY_EMAIL_OK: string, USER_ACCOUNT_ACTIVATED: string}, ERROR: {NO_TREE_FOUND: string, NO_TREE_TYPE_FOUND: string, NO_DATA: string, USER_NOT_ACTIVE: string, NO_SECURITY_GROUP_FOUND: string, INVALID_USER_PASSWORD: string, NO_TRANSACTION_TYPE_FOUND: string, NO_TRANSACTION_FOUND: string, TOKEN_INVALID: string, INVALID_AUTH_CREDENTIALS: string, NO_USER_FOUND: string, NO_USER_GROUP_FOUND: string, NO_DATA_FOUND: string, NO_IMAGE_FOUND: string, NO_TRANSACTION_METHOD_FOUND: string, ENCRYPTION_INVALID: string}}}
  */
 const MESSAGE = {
     ERROR: {
@@ -49,9 +49,18 @@ const MESSAGE = {
         NO_TRANSACTION_METHOD_FOUND: 'No transaction method found!',
         NO_TRANSACTION_FOUND: 'No transaction(s) found!',
         NO_SECURITY_GROUP_FOUND: 'No security group found!',
+        USER_NOT_ACTIVE: 'User not active!',
+        INVALID_AUTH_CREDENTIALS: 'Invalid authorization credentials!',
+        INVALID_USER_PASSWORD: 'Invalid user password!',
+        TOKEN_INVALID: 'Invalid token,login again!',
+        ENCRYPTION_INVALID: 'Encryption validation mismatch!',
     },
     GENERIC: {
-        INPUT_EXISTING_IMAGE: 'Enter existing image id!'
+        INPUT_EXISTING_IMAGE: 'Enter existing image id!',
+        USER_LOGOUT: 'User logged out!',
+        USER_ACCOUNT_ACTIVATED: 'User account activated!',
+        USER_PASSWORD_RECOVERY_EMAIL: 'User password recovery email sent!',
+        USER_PASSWORD_RECOVERY_EMAIL_OK: 'User password recovery completed successfully!',
     }
 }
 
